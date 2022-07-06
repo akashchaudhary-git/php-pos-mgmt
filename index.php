6f1bece2aa578460a622dddd51040615a3b2464d
Akash Chaudhary <akash.chaudhary@live.in>
2022-07-06 19:06:30 +0530

    Added Dashboard page, Header and Footer PHP

--- index.php
+++ index.php
@@ -16,13 +16,13 @@
     <link rel="stylesheet" href="./dist/css/adminlte.min.css">
 </head>
 
-<body class="hold-transition login-page">
+<body class="hold-transition bg-warning login-page">
     <div class="login-box">
         <div class="login-logo">
             <a href="./index.php"><b>POS</b>Management</a>
         </div>
         <!-- /.login-logo -->
-        <div class="card">
+        <div class="card shadow rounded">
             <div class="card-body login-card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
 
@@ -44,39 +44,21 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember">
-                                <label for="remember">
-                                    Remember Me
-                                </label>
-                            </div>
-                        </div>
+
                         <!-- /.col -->
-                        <div class="col-4">
+                        <div class="col-12">
                             <button type="submit" class="btn btn-primary btn-block">Sign In</button>
                         </div>
                         <!-- /.col -->
                     </div>
                 </form>
 
-                <div class="social-auth-links text-center mb-3">
-                    <p>- OR -</p>
-                    <a href="#" class="btn btn-block btn-primary">
-                        <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                    </a>
-                    <a href="#" class="btn btn-block btn-danger">
-                        <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                    </a>
-                </div>
-                <!-- /.social-auth-links -->
 
-                <p class="mb-1">
+
+                <p class="mb-1 mt-3">
                     <a href="#">I forgot my password</a>
                 </p>
-                <p class="mb-0">
-                    <a href="#" class="text-center">Register a new membership</a>
-                </p>
+
             </div>
             <!-- /.login-card-body -->
         </div>

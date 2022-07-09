83fd6c8e9a8b3c644bf68300503d29cf8ed814d4
Akash Chaudhary <akash.chaudhary@live.in>
2022-07-09 22:18:58 +0530

    .

--- admin/header.php
+++ admin/header.php
@@ -131,7 +131,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-warning elevation-4">
             <!-- Brand Logo -->
-            <a href="#" class="brand-link">
+            <a href="dashboard.php" class="brand-link">
                 <img src="../dist/img/AdminLTELogo.png" alt="POS Management" class="brand-image img-circle elevation-3" style="opacity: 0.8" />
                 <span class="brand-text font-weight-light">POS Management</span>
             </a>
